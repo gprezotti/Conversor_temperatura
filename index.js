dad5f2b38b88438c283ad530460f98215c8f0141
Gustavo Prezotti <90994324+gprezotti@users.noreply.github.com>
2024-05-09 10:35:05 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-function numero() {
-  let numero_aleatorio = Number(Math.floor(Math.random() * 11))
-  let numero_informado = Number(prompt("Informe um número entre 0 e 10:"))
-  
-  if (numero_informado == numero_aleatorio) {
-    alert("Você acertou o número. Parabéns!!")
-  }
-  else if ((numero_informado - numero_aleatorio == 1) || (numero_aleatorio - numero_informado == 1)) {
-    alert("Continue tentando, Você errou por um número! O número sorteado foi " + numero_aleatorio)
+function calcularTemperatura() {
+  let temperatura_celsius = prompt("Informe a temperatura em Celsius:")
+  let temperatura_farenheit = temperatura_celsius * 1.8 + 32
+  if (isNaN(temperatura_celsius))
+  {
+    alert("O argumento é inválido. Experimente digitar um número.")
   }
   else
   {
-    alert("Você errou. O número sorteado foi " + numero_aleatorio)
+    alert("A temperatura em Farenheit é " + temperatura_farenheit)
   }
 }
